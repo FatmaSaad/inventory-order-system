9186772fde0f8195a6e21a098f313a75f777ca92
FatmaSaad <fatmasaad313@gmail.com>
2024-12-17 22:41:58 +0200

    Update CreateOrderRequest.php

--- app/Http/Requests/CreateOrderRequest.php
+++ app/Http/Requests/CreateOrderRequest.php
@@ -54,29 +54,30 @@ class CreateOrderRequest extends FormRequest
      */
     private function checkStockAvailability($validator)
     {
-        foreach ($this->input('products') as $productOrder) {
-            // Check if the product_id and quantity are present
-            if (!isset($productOrder['product_id']) || !isset($productOrder['quantity'])) {
-                continue; // Skip this product if product_id or quantity is missing
-            }
-    
-            $product = Product::find($productOrder['product_id']);
-    
-            if ($product) {
-                foreach ($product->ingredients as $ingredient) {
-                    // Calculate the consumed quantity based on the product quantity
-                    $consumed = $ingredient->pivot->quantity * $productOrder['quantity'];
-    
-                    // Check if there is sufficient stock
-                    if ($ingredient->stock < $consumed) {
-                        $validator->errors()->add(
-                            'products.' . $productOrder['product_id'] . '.quantity',
-                            'Insufficient stock for ingredient: ' . $ingredient->name
-                        );
+        if ($this->input('products')) {
+            foreach ($this->input('products') as $productOrder) {
+                // Check if the product_id and quantity are present
+                if (!isset($productOrder['product_id']) || !isset($productOrder['quantity'])) {
+                    continue; // Skip this product if product_id or quantity is missing
+                }
+
+                $product = Product::find($productOrder['product_id']);
+
+                if ($product) {
+                    foreach ($product->ingredients as $ingredient) {
+                        // Calculate the consumed quantity based on the product quantity
+                        $consumed = $ingredient->pivot->quantity * $productOrder['quantity'];
+
+                        // Check if there is sufficient stock
+                        if ($ingredient->stock < $consumed) {
+                            $validator->errors()->add(
+                                'products.' . $productOrder['product_id'] . '.quantity',
+                                'Insufficient stock for ingredient: ' . $ingredient->name
+                            );
+                        }
                     }
                 }
             }
         }
     }
-    
 }
